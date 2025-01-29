a431857c096726673c0c598cd6d0e50ad25ad097
matteodragotto <matteodrag8@gmail.com>
2025-01-29 15:31:56 +0100

    [feat] section titles now show only when there are elements inside their sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,18 @@
 import MovieLists from "../components/MovieLists"
 import TvLists from "../components/TvLists"
+import { useGlobalContext } from "../context/GlobalContext"
 
 const HomePage = () => {
+
+  const { searchResults } = useGlobalContext()
   return (
     <div className="container x-5">
       <div className="row">
-        <h1>Movies</h1>
+        <h1>{searchResults.length === 0 ? '' : 'Movies'}</h1>
         <MovieLists />
       </div>
       <div className="row">
-        <h1>TV Series</h1>
+        <h1>{searchResults.length === 0 ? '' : 'TV Series'}</h1>
         <TvLists />
       </div>
 
